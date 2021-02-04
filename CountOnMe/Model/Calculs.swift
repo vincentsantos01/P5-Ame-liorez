ca66fca173470cd74da5c6e202d67ede092a9d40
vincent santos <61383060+vincentsantos01@users.noreply.github.com>
2021-02-04 11:11:28 +0100

    maj

--- CountOnMe/Model/Calculs.swift
+++ CountOnMe/Model/Calculs.swift
@@ -10,6 +10,14 @@ import Foundation
 class Calculator {
     var calculText: ((String) -> Void)?
     var displayAlert: ((String) -> Void)?
+    init() {
+            self.calculString = ""
+        }
+    var calculString: String {
+            didSet {
+                calculText?(calculString)
+            }
+        }
     var expression: String = "1 + 1 = 2" {
         didSet {
             calculText?(expression)
@@ -52,8 +60,72 @@ class Calculator {
             displayAlert?("valeurs manquantes")
             return
         }
-        
-        var operationsToReduce = elements
+       
+                var operationsToReduce = elements
+                
+                while operationsToReduce.count > 1 {
+                    
+                    guard var left = Double(operationsToReduce[0]) else { return }
+                   var operand = operationsToReduce[1]
+                    guard var right = Double(operationsToReduce[2]) else { return }
+                    
+                    let result: Double
+                        
+                        
+                        var operandIndex = 1
+                        
+                        
+                        if let index = operationsToReduce.firstIndex(where: { $0 == "x" || $0 == "÷" }) {
+                            
+                            operandIndex = index
+                            if let leftunwrapp = Double(operationsToReduce[index - 1]) { left = leftunwrapp }
+                            operand = operationsToReduce[index]
+                            if let rightUnwrapp = Double(operationsToReduce[index + 1]) { right = rightUnwrapp }
+                        }
+                        
+                        result = calculate(left: Double(left), right: Double(right), operand: operand)
+                        
+                    
+                        
+                        for _ in 1...3 {
+                            
+                            operationsToReduce.remove(at: operandIndex - 1)
+                         
+                        }
+                    
+                        operationsToReduce.insert(formatResult(result: Double(result)), at: operandIndex - 1 )
+                        
+                    }
+                    guard let finalResult = operationsToReduce.first else { return }
+                    calculString.append(" = \(finalResult)")
+            }
+    func calculate(left: Double, right: Double, operand: String) -> Double {
+               
+               let result: Double
+               switch operand {
+               case "+": result = left + right
+               case "-": result = left - right
+               case "÷": result = left / right
+               case "x": result = left * right
+               default: return 0.0
+               }
+               return result
+           }
+    private func formatResult(result: Double) -> String {
+         let formatter = NumberFormatter()
+         formatter.maximumFractionDigits = 3
+         guard let resultFormated = formatter.string(from: NSNumber(value: result)) else { return String() }
+         
+   
+         guard resultFormated.count <= 10 else {
+             return String(result)
+         }
+         return resultFormated
+     }
+    func clean() {
+            expression = ""
+        }
+       /* var operationsToReduce = elements
         while operationsToReduce.count > 1 {
             let left = Int(operationsToReduce[0])!
             let operand = operationsToReduce[1]
@@ -73,5 +145,5 @@ class Calculator {
     }
     func clean() {
             expression = ""
-        }
+        }*/
 }
